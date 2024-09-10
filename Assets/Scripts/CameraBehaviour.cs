0fe7771e19e3a75f3fd3aa01c503c982076b719e
Dan <dan.cheype@gmail.com>
2024-09-10 17:04:49 +0200

    [ADD] Input bindings

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,16 +4,18 @@ public class CameraBehaviour : MonoBehaviour
 {
     [Space]
     [Header("Forward Movement")]
-    [SerializeField] private float speedMultiplier = 1.0f;
+    [Range(0.0f, 1.0f)]
     [SerializeField] private float speedIncrementation = 1.0f;
     [SerializeField] private float progressiveSpeedMultiplier = 1.0f;
     [SerializeField] private float slowDownFactor = 1.0f;
+    
+    private float _speedMultiplier = 1.0f;
 
     void Update()
     {
-        speedMultiplier += Time.deltaTime / speedIncrementation;
-        speedMultiplier = Mathf.Clamp(speedMultiplier, 3, 5);
+        _speedMultiplier += Time.deltaTime / speedIncrementation;
+        _speedMultiplier = Mathf.Clamp(_speedMultiplier, 3, 5);
 
-        transform.position += Time.deltaTime * 4 * speedMultiplier * new Vector3(1, 0, 0) / slowDownFactor * progressiveSpeedMultiplier;
+        transform.position += Time.deltaTime * 4 * _speedMultiplier * new Vector3(1, 0, 0) / slowDownFactor * progressiveSpeedMultiplier;
     }
-}   
+}
