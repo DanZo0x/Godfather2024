147cfd29540fb9434cc67f652e410573e0bddf32
nessie134 <ines-caps@hotmail.com>
2024-09-12 15:25:03 +0200

    [MODIF] leaderboard avec meilleurs scores (à tester)

--- Assets/GP/Scripts/Leaderboard.cs
+++ Assets/GP/Scripts/Leaderboard.cs
@@ -5,20 +5,37 @@ using UnityEngine.UI;
 
 public class Leaderboard : MonoBehaviour
 {
-    private HightScoreData scoreData;
+    private HighScoreData scoreData;
     private string _filePath;
     public int value;
     public Text _score;
+    public Text[] scoreTexts;
 
     public static Leaderboard instance;
 
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
         _filePath = Application.persistentDataPath + "/HightScore.json";
         if (System.IO.File.Exists(_filePath))
         {
             Load();
         }
+        else
+        {
+            scoreData = new HighScoreData(); // Initialize in case file doesn't exist
+            scoreData.scores = new int[0]; // Initialize empty score array
+        }
+
+        UpdateLeaderboardUI();
     }
 
     private void Start()
@@ -56,18 +73,58 @@ public class Leaderboard : MonoBehaviour
             scoreData.scores[i] = saveInt[i]; 
         }
         scoreData.scores[scoreData.scores.Length-1] = value;
+
+        // Trie les scores en ordre décroissant
+        System.Array.Sort(scoreData.scores);
+        System.Array.Reverse(scoreData.scores);
+
         string save = JsonUtility.ToJson(scoreData);
         System.IO.File.WriteAllText(_filePath, save);
+
+        // Met à jour l'affichage des meilleurs scores
+        UpdateLeaderboardUI();
     }
     public void Load()
     {
-        string save = System.IO.File.ReadAllText(_filePath);
-        scoreData = JsonUtility.FromJson<HightScoreData>(save);
+        if (System.IO.File.Exists(_filePath))  // Check if file exists before loading
+        {
+            string save = System.IO.File.ReadAllText(_filePath);
+            scoreData = JsonUtility.FromJson<HighScoreData>(save);
+        }
+        else
+        {
+            scoreData = new HighScoreData(); // Initialize if file doesn't exist
+            scoreData.scores = new int[0];
+
+            // Met à jour les meilleurs scores affichés après chargement
+            UpdateLeaderboardUI();
+        }
+    }
+
+    // Méthode pour mettre à jour l'affichage des meilleurs scores
+    public void UpdateLeaderboardUI()
+    {
+        // Trie les scores en ordre décroissant
+        System.Array.Sort(scoreData.scores);
+        System.Array.Reverse(scoreData.scores);
+
+        // Met à jour les champs de texte pour afficher les scores
+        for (int i = 0; i < scoreTexts.Length; i++)
+        {
+            if (i < scoreData.scores.Length)
+            {
+                scoreTexts[i].text = (i + 1) + ". " + scoreData.scores[i].ToString();
+            }
+            else
+            {
+                scoreTexts[i].text = (i + 1) + ". ---";  // Si pas de score, affiche "---"
+            }
+        }
     }
 }
 
 [System.Serializable]
-public class HightScoreData
+public class HighScoreData
 {
     public int[] scores;
 }
