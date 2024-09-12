7a6a5b56c7217252ae4c6ec6616afe738ed279a8
Tevadu52 <tevafoug@gmail.com>
2024-09-12 11:34:01 +0200

    [ADD] Drone

--- Assets/GP/PlayerMovement.cs
+++ Assets/GP/PlayerMovement.cs
@@ -1,35 +1,51 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private BoxCollider2D player1;
+    [SerializeField] private BoxCollider2D playerCollider;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private GameObject target;
     
     [Space]
     [Header("Checks")]
     [SerializeField] private bool canMoveVertical = true;
-    
-    [Space]
-    [Header("Forward Movement")]
-    [SerializeField] private float speedMultiplier = 1.0f;
-    [SerializeField] private float progressiveSpeedMultiplier = 1.0f;
-    [SerializeField] private float slowDownFactor = 1.0f;
+    public bool canMove = true;
 
     private Vector2 _movementVector;
     [Range(0.0f, 1.0f)]
     [SerializeField] private float robotVelocity = 1.0f;
-    
+
+
+    [Space]
+    [Header("Movement Block")]
+    [SerializeField] private GameObject blockCenter;
+    [SerializeField] private float blockUp;
+    [SerializeField] private float blockDown;
+    [SerializeField] private float blockLeft;
+    [SerializeField] private float blockRight;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(blockCenter.transform.position + new Vector3(0, blockUp), .2f);
+        Gizmos.DrawSphere(blockCenter.transform.position + new Vector3(0, blockDown), .2f);
+        Gizmos.DrawSphere(blockCenter.transform.position + new Vector3(blockLeft, 0), .2f);
+        Gizmos.DrawSphere(blockCenter.transform.position + new Vector3(blockRight, 0), .2f);
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        player1 = GetComponent<BoxCollider2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        if(_movementVector.sqrMagnitude > 0.1f)
+        if(_movementVector.sqrMagnitude > 0.1f && canMove)
         {
             Move();
         }
@@ -67,7 +83,16 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.performed)
         {
-            
+            target.GetComponent<PlayerMovement>().canMove = canMove;
+            canMove = !canMove;
+            if (playerInput.currentActionMap == playerInput.actions.FindActionMap("Player"))
+            {
+                playerInput.SwitchCurrentActionMap("Drone");
+            }
+            else
+            {
+                playerInput.SwitchCurrentActionMap("Player");
+            }
         }
     }
 
@@ -76,6 +101,10 @@ public class PlayerMovement : MonoBehaviour
         float moveY = transform.position.y;
         if (canMoveVertical) moveY += _movementVector.y * robotVelocity;
         float moveX = transform.position.x + _movementVector.x * robotVelocity;
+        if (moveX > blockCenter.transform.position.x + blockLeft) moveX = blockCenter.transform.position.x + blockLeft;
+        if (moveX < blockCenter.transform.position.x + blockRight) moveX = blockCenter.transform.position.x + blockRight;
+        if (moveY > blockCenter.transform.position.y + blockUp) moveY = blockCenter.transform.position.y + blockUp;
+        if (moveY < blockCenter.transform.position.y + blockDown) moveY = blockCenter.transform.position.y + blockDown;
         Vector2 newPosVector = new Vector2(moveX, moveY);
         rb.MovePosition(newPosVector);
     }
