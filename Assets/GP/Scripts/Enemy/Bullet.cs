a9457b3a6fa8444bd2351f1e1f7aa8aed54c7e03
Tevadu52 <tevafoug@gmail.com>
2024-09-13 10:47:53 +0200

    [ADD] Bullet blink

--- Assets/GP/Scripts/Enemy/Bullet.cs
+++ Assets/GP/Scripts/Enemy/Bullet.cs
@@ -9,10 +9,15 @@ public class Bullet : MonoBehaviour
     private float timer = 0;
     private Rigidbody2D rb;
     private Vector3 direction;
+    [SerializeField] private bool enemy = false;
+    [SerializeField] private Color flashColor = Color.yellow; // Couleur du flash
+    [SerializeField] private float flashDuration = 0.1f; // Durée du flash
+    private SpriteRenderer spriteRenderer; // Référence au SpriteRenderer de l'obstacle
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void Init(float bulletSpeed, float bulletLifeTime, Vector3 target)
@@ -21,6 +26,7 @@ public class Bullet : MonoBehaviour
         lifeTime = bulletLifeTime;
         direction = target - transform.position;
         rb.velocity = direction * speed;
+        StartCoroutine(FlashAndDeactivate());
     }
 
     void Update()
@@ -33,5 +39,22 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
+    }
+    private IEnumerator FlashAndDeactivate()
+    {
+        if (spriteRenderer != null && enemy)
+        {
+            Color originalColor = spriteRenderer.color; // Sauvegarder la couleur d'origine
+
+            // Changer la couleur en couleur flash
+            spriteRenderer.color = flashColor;
+            yield return new WaitForSeconds(flashDuration);
+
+            // Remettre la couleur d'origine
+            spriteRenderer.color = originalColor;
+
+            yield return new WaitForSeconds(flashDuration);
+            StartCoroutine(FlashAndDeactivate());
+        }
     }
 }
